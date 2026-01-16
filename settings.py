2bc11f1d6de65e6ea140023dd4e5b40b575b47fd
Paweł <htm52004@gmail.com>
2026-01-16 21:32:45 +0100

    Dodana wstepna mechanika do balonow

--- settings.py
+++ settings.py
@@ -1,2 +1,6 @@
 WIDTH, HEIGHT = 1280, 720
-DIFFICULTIES = {}
+DIFFICULTIES = {
+    "Easy": {"hp_mult": 0.8, "speed_mult": 0.9, "reward_mult": 1.2},
+    "Normal": {"hp_mult": 1.0, "speed_mult": 1.0, "reward_mult": 1.0},
+    "Hard": {"hp_mult": 1.5, "speed_mult": 1.2, "reward_mult": 0.8}
+}
